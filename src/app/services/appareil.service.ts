a79f803fbaee8571b18f0ed25e7672a6ae349c88
dlavaury <dlavaury@student.42.fr>
2018-11-15 11:21:01 +0100

    navigation rooting

--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -1,19 +1,31 @@
 export class AppareilService {
     appareils = [
         {
-          name: 'Machine à laver',
-          status: 'allumé'
+            id: 1,
+            name: 'Machine à laver',
+            status: 'allumé'
         },
         {
-          name: 'Télévision',
-          status: 'allumé'
+            id: 2,
+            name: 'Télévision',
+            status: 'allumé'
         },
         {
-          name: 'Ordinateur',
-          status: 'éteint'
+
+            id: 3,
+            name: 'Ordinateur',
+            status: 'éteint'
         }
     ];
 
+    getAppareilById(id: number) {
+        const appareil = this.appareils.find(
+            (appareilObject) => {
+                return appareilObject.id === id;
+            }
+        );
+        return appareil;
+    }
     switchOnAll() {
         for (const appareil of this.appareils) {
             appareil.status = 'allumé';
