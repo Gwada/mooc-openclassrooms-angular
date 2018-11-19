043233db5d942c6eb8954414cefac6904513172d
dlavaury <dlavaury@student.42.fr>
2018-11-19 17:05:37 +0100

    bookshelves 1st part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,17 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class AppComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor() {
+    const config = {
+      apiKey: 'AIzaSyApgChuzZ7QmXQh1iojKeurbWsUnGYiSLg',
+      authDomain: 'bookshelves-cf3b1.firebaseapp.com',
+      databaseURL: 'https://bookshelves-cf3b1.firebaseio.com',
+      projectId: 'bookshelves-cf3b1',
+      storageBucket: 'bookshelves-cf3b1.appspot.com',
+      messagingSenderId: '349381707022'
+    };
+    firebase.initializeApp(config);
+  }
 
   ngOnInit() {
   }
